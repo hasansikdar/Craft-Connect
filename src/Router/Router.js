de5400881a07ab80d70b65adfd3f9c987859d4c8
Mamun8854 <mamunabdullah8854@gmail.com>
2023-01-10 23:23:59 +0600

    login form added in login route

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../Pages/Login/Login";
 import PostBox from "../Pages/PostBox/PostBox";
 
 export const Routes = createBrowserRouter([
-    {path: '/', element: <PostBox/>}
-])
+  { path: "/", element: <PostBox /> },
+  { path: "/login", element: <Login></Login> },
+]);
