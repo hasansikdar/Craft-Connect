30b5ea881edad1e2f936e22f642251e376f103d5
hasansikdar <hasansikdar973@gmail.com>
2023-01-11 17:46:44 +0600

    profile added

--- src/Components/PostCard/PostUserInfo/PostUserInfo.js
+++ src/Components/PostCard/PostUserInfo/PostUserInfo.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { FaThumbtack, FaRegSave, FaCommentAlt, FaEdit, } from "react-icons/fa";
 const PostUserInfo = () => {
     return (
-        <div className='flex justify-between'>
-            {/* <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" /> */}
-            <div>
+        <div className='flex'>
+            <img className='w-10 h-10 rounded-full' src="https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/318630834_1301786737319944_3904799583444523390_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFjKxLtWVgY0EcruBuIVQC4QW8Nr8xSuHNBbw2vzFK4c37HruuJsS6Hhy1MU_2h_0URlH6e25P0qxtME-zNc4ri&_nc_ohc=rFDSVULP1gUAX_u9RLZ&_nc_ht=scontent.fdac5-2.fna&oh=00_AfDUyor27xgdIX_rgzNZH6VdvD1Pl-1HDtPeEPny2fepjw&oe=63C2B9A3" alt="" />
+            <div className='ml-4'>
                 <a className='hover:underline' href='#'>Muhammad Hasan</a><br></br>
                 <div className='flex items-center'>
                     <a className='hover:underline' href="#">5 Nov 2023</a>
@@ -14,7 +14,7 @@ const PostUserInfo = () => {
                     </label>
                 </div>
             </div>
-            <div>
+            <div className='ml-auto'>
                 <div className="dropdown dropdown-bottom dropdown-end">
                     <label tabIndex={0} className="btn  btn-sm">...</label>
                     <div tabIndex={0} className="dropdown-content  menu px-3 bg-[black] shadow bg-base-100 rounded-box w-52 py-5">
