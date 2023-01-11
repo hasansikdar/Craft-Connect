4936541d29065aa003c442ddcb1f262147e58b73
farhanchy99 <farhanchowdhury1999@gmail.com>
2023-01-11 19:31:44 +0600

    Farhan Rightbar

--- src/Shared/Right-Side-Bar/RightBar/RightBar.js
+++ src/Shared/Right-Side-Bar/RightBar/RightBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Advertisement from '../Advertisement/Advertisement';
 import FriendsList from '../FriendsList/FriendsList';
 import ProfileDetails from '../ProfileDetails/ProfileDetails';
 
 const RightBar = () => {
     return (
-        <div className='w-80 float-right bg-slate-100'>
+        <div className='w-80 float-right bg-slate-100 h-full'>
             <ProfileDetails></ProfileDetails>
             <FriendsList></FriendsList>
-            <FriendsList></FriendsList>
+            <Advertisement></Advertisement>
         </div>
     );
 };
