58301c787c6de202bed9e8b9ac9885145d0ff6ba
maruf45 <armaruf450@gmail.com>
2023-01-11 17:38:14 +0600

    update stories and some animtaions

--- src/Pages/Stories/Stories.js
+++ src/Pages/Stories/Stories.js
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PostBox from "../PostBox/PostBox";
 import stories from "./stories.json";
+import { motion } from "framer-motion";
 const Stories = () => {
+  const [width, setWidth] = useState();
+  const storyWidth = useRef();
+  useEffect(() => {
+    setWidth(storyWidth.current.scrollWidth - storyWidth.current.offsetWidth);
+  }, []);
   return (
     <>
-      <div className="stories-container container mx-auto bg-zinc-700 rounded-md mt-5 p-5 text-white">
-        <h1 className="text-2xl font-bold py-2 mx-auto">Stories</h1>
-        <div className="stories flex gap-3 justify-center overflow-x-auto">
+      <motion.div className="mx-auto container  bg-zinc-700 rounded-md mt-5 p-5 overflow-hidden text-white w-[50%]">
+        <h1 className="text-2xl font-bold py-4 pb-3 text-center ">Stories</h1>
+        <motion.div
+          drag="x"
+          ref={storyWidth}
+          dragConstraints={{ right: 0, left: -width }}
+          whileTap={{cursor: 'grabbing'}}
+          className="stories flex gap-3 cursor-grab"
+        >
           {stories.map((sto) => {
             return (
               <>
-                <div
-                  className="bg-img w-[180px] h-[300px] bg-zinc-800 rounded-md hover:scale-[1.02] transition bg-center bg-cover"
+                <motion.div
+                  className="bg-img min-w-[180px] h-[300px] bg-zinc-800 rounded-md transition bg-center bg-cover"
                   style={{
                     background: `linear-gradient(to top, rgba(0,0,0,0.5), rgba(0,0,0,0.2)) , url(${sto.userStory})`,
                     "background-position": "center",
                     "background-size": "cover",
                   }}
                 >
-                  <div className="flex flex-col justify-between h-full rounded-md p-4 cursor-pointer">
+                  <motion.div className="flex flex-col justify-between h-full rounded-md p-4">
                     <img
                       src={sto.avatar}
                       className="h-12 w-12 rounded-full"
                       alt=""
                     />
                     <p>{sto.userName}</p>
-                  </div>
-                </div>
+                  </motion.div>
+                </motion.div>
               </>
             );
           })}
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
       <PostBox />
     </>
   );
