d28833b32f5119b9efdda2e63869466cbc564227
maj-jinnah <maj.jinnah@gmail.com>
2023-01-11 10:02:53 +0600

    start project

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,5 +2,5 @@ import { createBrowserRouter } from "react-router-dom";
 import PostBox from "../Pages/PostBox/PostBox";
 
 export const Routes = createBrowserRouter([
-    {path: '/', element: <PostBox/>}
+    { path: '/', element: <PostBox /> }
 ])
