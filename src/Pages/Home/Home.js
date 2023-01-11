27fa05dd714243151494bca4bb1e5d81ec2b4934
hasansikdar <hasansikdar973@gmail.com>
2023-01-11 08:29:45 +0600

    some problem fixed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import LeftNav from '../../Shared/LeftNav/LeftNav';
 import RightBar from '../../Shared/Right-Side-Bar/RightBar/RightBar';
-import PostBox from '../PostBox/PostBox';
 
 const Home = () => {
     return (
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
 import PostBox from "../Pages/PostBox/PostBox";
 
 export const Routes = createBrowserRouter([
-    {path: '/', element: <PostBox/>}
+    {path: '/postbox', element: <PostBox/>},
+    {path: '/', element: <Home></Home>}
 ])
