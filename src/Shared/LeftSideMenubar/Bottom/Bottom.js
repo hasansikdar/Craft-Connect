c1bcfa03d479a154567240f9971f84278e7ce502
maj-jinnah <maj.jinnah@gmail.com>
2023-01-12 17:06:32 +0600

    task is done

--- src/Shared/LeftSideMenubar/Bottom/Bottom.js
+++ src/Shared/LeftSideMenubar/Bottom/Bottom.js
@@ -9,6 +9,18 @@ const Bottom = () => {
                     <img src="https://placeimg.com/192/192/people" alt='' className='w10 h-10 rounded-full'/>
                     <h1 className='text-lg font-semibold'>Congratulations your page is here!</h1>
                 </div>
+                <div className='flex justify-start items-center gap-4 mb-4'>
+                    <img src="https://placeimg.com/192/192/people" alt='' className='w10 h-10 rounded-full'/>
+                    <h1 className='text-lg font-semibold'>Congratulations your page is here!</h1>
+                </div>
+                <div className='flex justify-start items-center gap-4 mb-4'>
+                    <img src="https://placeimg.com/192/192/people" alt='' className='w10 h-10 rounded-full'/>
+                    <h1 className='text-lg font-semibold'>Congratulations your page is here!</h1>
+                </div>
+                <div className='flex justify-start items-center gap-4 mb-4'>
+                    <img src="https://placeimg.com/192/192/people" alt='' className='w10 h-10 rounded-full'/>
+                    <h1 className='text-lg font-semibold'>Congratulations your page is here!</h1>
+                </div>
             </div>
         </div>
     );
