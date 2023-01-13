4e6dd2de9c5ce6911e2de8bb673deace585b9ce2
Mamun8854 <mamunabdullah8854@gmail.com>
2023-01-13 17:51:07 +0600

    Updated

--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PostCard from '../../Components/PostCard/PostCard';
+import React from "react";
+import PostCard from "../../Components/PostCard/PostCard";
 
 const Posts = () => {
-    return (
-        <div className='justify-center flex py-10'>
-            <PostCard></PostCard>
-        </div>
-    );
+  return (
+    <div className="justify-center flex py-10">
+      <PostCard></PostCard>
+    </div>
+  );
 };
 
 export default Posts;
