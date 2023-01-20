f0cbec1713fc15890c943645cf6f98448dbddcb2
ifty17 <iftyrahatkabir@gmail.com>
2023-01-20 15:45:19 +0600

    added three dots button

--- src/Pages/UserProfile/OptionsProfile.js
+++ src/Pages/UserProfile/OptionsProfile.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { BsThreeDots } from "react-icons/bs";
 
 const OptionsProfile = () => {
     return (
       <div className="w-[945px] mx-auto mt-32 border-y-[1px]">
-        
-        <div className="flex items-center justify-evenly ">
-          <button>Posts</button>
-          <button>About</button>
-          <button>Friends</button>
-          <button>Photos</button>
-          <button>Videos</button>
-          <button>Check-Ins</button>
+        <div className='flex justify-between py-2'>
+          <div className="flex w-full items-center justify-start gap-[60px] ml-[10px]">
+            <button>Posts</button>
+            <button>About</button>
+            <button>Friends</button>
+            <button>Photos</button>
+            <button>Videos</button>
+            <button>Check-Ins</button>
+          </div>
+          <button>
+            <BsThreeDots className="text-3xl rounded-lg p-1 bg-gray-400 text-black" />
+          </button>
         </div>
-        
       </div>
     );
 };
