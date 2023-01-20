758971d65ed500b1ade7dea4e41175d71742573e
ifty17 <iftyrahatkabir@gmail.com>
2023-01-20 15:54:50 +0600

    added cover img

--- src/Pages/UserProfile/Cover.js
+++ src/Pages/UserProfile/Cover.js
@@ -3,6 +3,7 @@ import LeftSideMobile from "../LeftSideBar/LeftSideMobile";
 import { BsCameraFill } from "react-icons/bs";
 import { FaPhotoVideo, FaUpload } from "react-icons/fa";
 import useClickOutside from '../../Components/helpers/clickOutside';
+import coverImg from '../../assets/cover.jpg'
 
 const Cover = () => {
 
@@ -18,6 +19,7 @@ const Cover = () => {
           </div>
           <div className="max-w-[945px] mx-auto mt-[60px]">
             <div className="relative h-[350px] w-[100%] rounded-b-lg bg-gray-600">
+                <img src={coverImg} className='h-[350px] w-full rounded-b-[10px]' alt="" />
               <div className="absolute bottom-4 right-4">
                 <div
                   onClick={() => setShowCoverMenu(!showCoverMenu)}
--- src/Pages/UserProfile/OptionsProfile.js
+++ src/Pages/UserProfile/OptionsProfile.js
@@ -3,10 +3,12 @@ import { BsThreeDots } from "react-icons/bs";
 
 const OptionsProfile = () => {
     return (
-      <div className="w-[945px] mx-auto mt-32 border-y-[1px]">
-        <div className='flex justify-between py-2'>
+      <div className="w-[945px] mx-auto mt-32 border-y-[1px] border-gray-400">
+        <div className="flex justify-between py-2">
           <div className="flex w-full items-center justify-start gap-[60px] ml-[10px]">
-            <button>Posts</button>
+            <div className="border-b-4 border-blue-500 px-[5px]">
+              <button className="">Posts</button>
+            </div>
             <button>About</button>
             <button>Friends</button>
             <button>Photos</button>
