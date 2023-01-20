434c908e5ed203bf03fc0f093619c86929995bba
hasansikdar <hasansikdar973@gmail.com>
2023-01-20 20:54:36 +0600

    final push

--- src/Pages/Groups/Groups.js
+++ src/Pages/Groups/Groups.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Posts from '../Posts/Posts';
 
 import GroupCard from './GroupCard/GroupCard';
 
@@ -9,9 +10,9 @@ const Groups = () => {
         //     <h1>This is a Groups page. Thanks</h1>
         // </div>
         <div className='flex mt-5'>
-            <div>
-                <GroupCard></GroupCard>
-            </div>
+            {/* <div>
+                <Posts></Posts>
+            </div> */}
             <div>
                 <ul className="menu bg-slate-100 shadow-lg w-72 text-black p-2 rounded-box">
                     <p className="font-medium text-lg pl-3 my-1">Gropes</p>
