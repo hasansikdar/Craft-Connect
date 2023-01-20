749cb92b3fd5b2769759f8541911b6ff380778fe
hasansikdar <hasansikdar973@gmail.com>
2023-01-20 18:10:30 +0600

    rahat and mamun final push

--- src/PrivateRouter/PrivateRouter.js
+++ src/PrivateRouter/PrivateRouter.js
@@ -2,15 +2,17 @@ import React, { Children, useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Authcontext } from '../Context/UserContext';
 
-const PrivateRouter = ({children}) => {
-    const {user, loading} = useContext(Authcontext);
+const PrivateRouter = ({ children }) => {
+    const { user, loading } = useContext(Authcontext);
 
-    if(loading){
-        return <h1 className='text-3xl text-center mt-5'>Loading...</h1>
+    if (loading) {
+        return <div className='text-3xl text-center flex items-center justify-center w-full text-black h-screen'>
+            <h1>Loading...</h1>
+        </div>
     }
 
 
-    if(user){
+    if (user) {
         return children;
     }
 
