fd2994bed5ee6b3b40eb11a0a589156be11ba913
Mamun8854 <mamunabdullah8854@gmail.com>
2023-01-22 21:31:40 +0600

    Custom middle  side  add

--- src/Shared/LeftSideMenubar/Friends/CustomList/CustomList.js
+++ src/Shared/LeftSideMenubar/Friends/CustomList/CustomList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../../../Pages/Navbar/Navbar";
+import CustomFriendProfile from "./CustomFriendProfile/CustomFriendProfile";
 import CustomLeftSide from "./CustomLeftSide/CustomLeftSide";
 
 const CustomList = () => {
@@ -7,11 +8,11 @@ const CustomList = () => {
     <section>
       <Navbar></Navbar>
       <div className="grid grid-cols-12 mt-[60px] bg-white dark:bg-black text-black dark:text-white max-w-screen-xl">
-        <div className="col-span-3 dark:bg-black h-screen">
+        <div className="col-span-3 dark:bg-black h-screen border-x">
           <CustomLeftSide></CustomLeftSide>
         </div>
         <div className="col-span-9">
-          <h2>Hello From Main Page</h2>
+          <CustomFriendProfile></CustomFriendProfile>
         </div>
       </div>
     </section>
