52348781e3f9841be60fec3ecca0fbdc5d8cb5a1
Mamun8854 <mamunabdullah8854@gmail.com>
2023-01-27 12:33:00 +0600

    home page left side responsive for mobile device

--- src/Pages/LeftSideBar/LeftSideMobile.js
+++ src/Pages/LeftSideBar/LeftSideMobile.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import {FaUserFriends} from "react-icons/fa";
-import { FcHome, FcAdvertising, FcBarChart,FcSteam, FcClapperboard, FcConferenceCall, FcNews } from "react-icons/fc";
+import React from "react";
+import { FaUserFriends } from "react-icons/fa";
+import {
+  FcHome,
+  FcAdvertising,
+  FcBarChart,
+  FcSteam,
+  FcClapperboard,
+  FcConferenceCall,
+  FcNews,
+} from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const LeftSideMobile = () => {
-    return (
-      <div className="">
-        <div className=" fixed border-r h-full">
-          <div className='mx-2'>
+  return (
+    <div className="">
+      <div className=" fixed border-r h-full">
+        <div className="mx-2">
+          <Link to="/">
             <FcHome className="text-3xl text-cyan-500" />
+          </Link>
+          <Link to="/friends">
             <FaUserFriends className="text-3xl text-cyan-500 my-6" />
-            <FcClapperboard className="text-3xl text-cyan-500 my-6" />
-            <FcNews className="text-3xl text-cyan-500 my-6" />
-            <FcConferenceCall className="text-3xl text-cyan-500 my-6" />
-            <FcHome className="text-3xl text-cyan-500 my-6" />
-            <FcSteam className="text-3xl text-cyan-500 my-6" />
-            <FcAdvertising className="text-3xl text-cyan-500 my-6" />
-            <FcBarChart className="text-3xl text-cyan-500 my-6" />
-          </div>
+          </Link>
+          <FcClapperboard className="text-3xl text-cyan-500 my-6" />
+          <FcNews className="text-3xl text-cyan-500 my-6" />
+          <FcConferenceCall className="text-3xl text-cyan-500 my-6" />
+          <FcHome className="text-3xl text-cyan-500 my-6" />
+          <FcSteam className="text-3xl text-cyan-500 my-6" />
+          <FcAdvertising className="text-3xl text-cyan-500 my-6" />
+          <FcBarChart className="text-3xl text-cyan-500 my-6" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default LeftSideMobile;
