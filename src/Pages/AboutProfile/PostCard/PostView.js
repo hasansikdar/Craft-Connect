efec58dae5607e66e3a800d4332f089b81ab4d48
ifty17 <iftyrahatkabir@gmail.com>
2023-01-27 17:09:16 +0600

    added reactions

--- src/Pages/AboutProfile/PostCard/PostView.js
+++ src/Pages/AboutProfile/PostCard/PostView.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import profileImg from '../../../assets/profile.jpg';
-import craftify from '../../../assets/jinnahPagePic/jinnahPagePic3.jpg'
-import { BsThreeDots } from "react-icons/bs";
+import PostCards from './PostCards';
+
 
 const PostView = () => {
     return (
@@ -11,36 +10,7 @@ const PostView = () => {
             <p>Hello </p>
           </div>
           <div>
-            <div className="w-[530px] border p-5 rounded-md shadow-md">
-              <div className='flex justify-between items-center'>
-                <div className="flex gap-3 items-center">
-                  <img
-                    className="w-[50px] rounded-full"
-                    src={profileImg}
-                    alt=""
-                  />
-                  <div>
-                    <p>Rahat Kabir Ifty</p>
-                    <p className="text-sm">19 hours ago</p>
-                  </div>
-                </div>
-                <div>
-                    <p className='text-4xl cursor-pointer'><BsThreeDots/></p>
-                </div>
-              </div>
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Deserunt libero magni tempore officiis amet hic rerum harum
-                  possimus eius, unde ea a accusantium saepe?
-                </p>
-                <img
-                  className="w-[488px] rounded-md mt-[5px]"
-                  src={craftify}
-                  alt=""
-                />
-              </div>
-            </div>
+            <PostCards/>
           </div>
           <div className="w-[251px] border">
             <p>Hello </p>
