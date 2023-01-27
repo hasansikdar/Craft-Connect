10a844c448314915ec75818283adda6f35e918bd
ifty17 <iftyrahatkabir@gmail.com>
2023-01-27 19:15:36 +0600

    added latest photos

--- src/Pages/AboutProfile/PostCard/PostView.js
+++ src/Pages/AboutProfile/PostCard/PostView.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import PostCards from './PostCards';
 import ProfileLeft from './ProfileLeft';
+import img1 from '../../../assets/ifty/last-phot11-large.webp';
+import img2 from '../../../assets/ifty/last-phot12-large.webp';
+import img3 from '../../../assets/ifty/last-phot13-large.webp';
+import img4 from '../../../assets/ifty/last-phot14-large.webp';
+import img5 from '../../../assets/ifty/last-phot15-large.webp';
+import img6 from '../../../assets/ifty/last-phot17-large.webp';
+import img7 from '../../../assets/ifty/last-phot18-large.webp';
+import img8 from '../../../assets/ifty/photo-1503965830912-6d7b07921cd1.avif';
+import img9 from '../../../assets/ifty/photo-1633467067670-30701ff2dcbd.avif';
 
 
 const PostView = () => {
     return (
       <div className=" ">
         <div className="flex gap-4 justify-center">
-          
-          <ProfileLeft/>
+          <ProfileLeft />
           <div>
             <PostCards />
           </div>
-          <div className="w-[251px] border hidden md:block">
-            <p>Hello </p>
+          <div className="w-[251px] p-5 border hidden md:block rounded-md">
+            <div className="border-b">
+              <p className="text-2xl font-bold">Latest Photos</p>
+            </div>
+            <div className='pt-5 grid grid-cols-3 gap-3'>
+                <div>
+                    <img className='w-[55px]' src={img1} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img2} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img3} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img4} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img5} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img6} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img7} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img8} alt="" />
+                </div>
+                <div>
+                    <img className='w-[55px]' src={img9} alt="" />
+                </div>
+            </div>
           </div>
         </div>
       </div>
