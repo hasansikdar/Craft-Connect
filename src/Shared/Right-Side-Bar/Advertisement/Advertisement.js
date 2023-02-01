4d8a33d05b07d34b15cd5e818627f8856c4495de
ifty17 <iftyrahatkabir@gmail.com>
2023-02-01 15:47:05 +0600

    updated navbar

--- src/Shared/Right-Side-Bar/Advertisement/Advertisement.js
+++ src/Shared/Right-Side-Bar/Advertisement/Advertisement.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import stories from "../../../Pages/Adevertisement/stories.json";
 
 const Advertisement = () => {
