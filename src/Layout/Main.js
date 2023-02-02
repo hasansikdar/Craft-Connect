17215eed4c5790751b9b9e67234f7ce6a53b89d5
farhanchy99 <farhanchowdhury1999@gmail.com>
2023-02-02 19:26:51 +0600

    fixing1

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,23 +1,48 @@
-import React from "react";
-import { Outlet } from "react-router";
+// import React from "react";
+// import { Outlet } from "react-router";
+// import Navbar from "../Pages/Navbar/Navbar";
+// import LeftSideBar from "../Shared/LeftSideMenubar/LeftSideBar/LeftSideBar";
+// import RightBar from "../Shared/Right-Side-Bar/RightBar/RightBar";
+
+// const Main = () => {
+//   return (
+//     <div className="dark:bg-[#2C2048] bg-white overflow-hidden h-screen">
+//       <Navbar></Navbar>
+//       <div className="lg:flex">
+//         {/* left site bar */}
+//         <div className="xl:mr-auto lg:w-[300px] mt-[58px]">
+//           <LeftSideBar></LeftSideBar>
+//         </div>
+//         <div className="lg:w-[600px] xl:mx-auto mt-[60px]">
+//           <Outlet></Outlet>
+//         </div>
+//         <div className="lg:ml-auto bg-zinc-800 rounded-lr-lg mt-[60px]">
+//           <RightBar></RightBar>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Main;
+
+
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Navbar from "../Pages/Navbar/Navbar";
-import LeftSideBar from "../Shared/LeftSideMenubar/LeftSideBar/LeftSideBar";
-import RightBar from "../Shared/Right-Side-Bar/RightBar/RightBar";
+import LeftSideBar from '../Shared/LeftSideMenubar/LeftSideBar/LeftSideBar'
 
 const Main = () => {
   return (
-    <div className="dark:bg-[#2C2048] bg-white overflow-hidden h-screen">
+    <div className="dark:bg-[#2C2048] bg-white">
       <Navbar></Navbar>
-      <div className="lg:flex">
-        {/* left site bar */}
-        <div className="xl:mr-auto lg:w-[300px] mt-[58px]">
+     
+      <div className='lg:flex gap-8'>
+        <div>
           <LeftSideBar></LeftSideBar>
         </div>
-        <div className="lg:w-[600px] xl:mx-auto mt-[60px]">
+        <div className=" mx-auto mt-[60px]">
           <Outlet></Outlet>
-        </div>
-        <div className="lg:ml-auto bg-zinc-800 rounded-lr-lg mt-[60px]">
-          <RightBar></RightBar>
         </div>
       </div>
     </div>
@@ -25,6 +50,3 @@ const Main = () => {
 };
 
 export default Main;
-
-
-
