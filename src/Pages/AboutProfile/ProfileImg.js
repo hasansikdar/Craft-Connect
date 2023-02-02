eca6392e367d2b5676cd80360b33bddf5a7b65c2
ifty17 <iftyrahatkabir@gmail.com>
2023-02-03 01:52:47 +0600

    added profile img in profile

--- src/Pages/AboutProfile/ProfileImg.js
+++ src/Pages/AboutProfile/ProfileImg.js
@@ -10,7 +10,7 @@ const ProfileImg = () => {
       <div className="absolute top-0 md:mt-[310px] md:mx-[490px]">
         <img
           className="w-[132px] h-[132px] rounded-full"
-          src={profileImg}
+          src={user?.photoURL}
           alt=""
         />
       </div>
