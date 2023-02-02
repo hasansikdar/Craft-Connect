84d01f5ea59f59257b273eb440dbe776cabfca81
maj-jinnah <maj.jinnah@gmail.com>
2023-02-02 10:29:27 +0600

    left sidebar is processing

--- src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
+++ src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
@@ -1,27 +1,11 @@
 
 //         <div className="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:bg-white hover:shadow-lg">
-//           <div className="flex h-screen flex-col justify-between pt-2 pb-6">
-//             <div>
-//               <div className="w-max p-2.5">
-//                 <img src="https://tailus.io/images/logo.svg" className="w-32" alt="" />
-//               </div>
-//               <ul className="mt-6 space-y-2 tracking-wide">
-//                 <li className="min-w-max">
-//                   <Link href="#" aria-label="dashboard" className="relative flex items-center space-x-4 bg-gradient-to-r from-sky-600 to-cyan-400 px-4 py-3 text-white">
-//                     <svg className="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
-//                       <path d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z" className="fill-current text-cyan-400 dark:fill-slate-600"></path>
-//                       <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z" className="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
-//                       <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z" className="fill-current group-hover:text-sky-300"></path>
-//                     </svg>
-//                     <span className="-mr-1 font-medium">Dashboard</span>
-//                   </Link>
-//                 </li>
-//                 <li className="min-w-max">
-//                   <Link href="#" className="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
-//                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-//                       <path className="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
-//                       <path className="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-//                     </svg>
+
+
+
+
+
+
 //                     <span className="group-hover:text-gray-700">Categories</span>
 //                   </Link>
 //                 </li>
