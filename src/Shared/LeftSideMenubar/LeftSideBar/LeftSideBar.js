4bca352dbaf38b7251b8cd1246db23a937a59d63
maj-jinnah <maj.jinnah@gmail.com>
2023-02-02 10:31:24 +0600

    sidebar is stail processing

--- src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
+++ src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
@@ -6,18 +6,7 @@
 
 
 
-//                     <span className="group-hover:text-gray-700">Categories</span>
-//                   </Link>
-//                 </li>
-//                 <li className="min-w-max">
-//                   <Link href="#" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-//                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-//                       <path className="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
-//                       <path className="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-//                     </svg>
-//                     <span className="group-hover:text-gray-700">Reports</span>
-//                   </Link>
-//                 </li>
+
 //                 <li className="min-w-max">
 //                   <Link href="#" className="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
 //                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
