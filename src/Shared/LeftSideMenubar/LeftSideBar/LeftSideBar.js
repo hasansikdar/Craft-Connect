15cc5551ef6cb165f303d69ffd9ea917fe12a7e3
maj-jinnah <maj.jinnah@gmail.com>
2023-02-02 10:23:31 +0600

    left sidebar is processing

--- src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
+++ src/Shared/LeftSideMenubar/LeftSideBar/LeftSideBar.js
@@ -1,7 +1,3 @@
-// import React from "react";
-// import Bottom from "../Bottom/Bottom";
-// import Top from "../Top/Top";
-
 // const LeftSideBar = () => {
 //   return (
 //     <div className="hidden overflow-x-hidden h-screen pb-20 lg:block w-[300px] p-3 dark:bg-zinc-800 dark:text-white bg-white text-black">
