da6a2bf75c2a3b66f5a06496108e3cfc432f5cac
farhanchy99 <farhanchowdhury1999@gmail.com>
2023-02-03 21:03:20 +0600

    design fix4

--- src/Shared/LeftSideMenubar/ProfileDetails/ProfileDetails.js
+++ src/Shared/LeftSideMenubar/ProfileDetails/ProfileDetails.js
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { HiSpeakerphone } from "react-icons/hi";
-import { FaEdit, FaWifi } from "react-icons/fa";
+import { FaEdit, FaWifi, FaUserCheck } from "react-icons/fa";
 import { Authcontext } from "../../../Context/UserContext";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
 
 const ProfileDetails = () => {
   const { user } = useContext(Authcontext);
   return (
     <div className="">
-      <div className="card-body p-2 flex rounded-lg border-zinc-600 shadow-xl w-[300px] text-gray-500 dark:bg-[#261b40] ">
+      <div className="card-body p-2 flex rounded-lg border-zinc-600 shadow-xl w-[300px] text-gray-500 dark:bg-[#261b40] overflow-scroll h-screen">
         <h1 className="ml-4 mt-4 dark:text-white text-black">Your Profile</h1>
         <div className="flex items-center flex-col">
           <img
@@ -41,10 +43,27 @@ const ProfileDetails = () => {
         </div>
         <div className="divider m-0"></div> 
         <div>
-            <Link className="justify-between flex dark:text-white text-gray-700 duration-200 rounded-md px-5 py-2">
-              <FaWifi></FaWifi> Followed by 487 people
+            <Link className="justify-between flex dark:text-white text-gray-700 duration-200 rounded-md px-2 py-2">
+            Followed by <span className="flex items-center"><FaWifi className="mr-2"></FaWifi> 487 people </span>
             </Link>
         </div>
+        <div>
+            <Link className="justify-between flex dark:text-white text-gray-700 duration-200 rounded-md px-2 py-2">
+            Friendlists <span className="flex items-center"><FaUserCheck className="mr-2"></FaUserCheck> 492 friends </span>
+            </Link>
+        </div>
+        <div className="divider m-0"></div>
+        <div className="px-2">
+          <p className="font-bold dark:text-white text-gray-700 ml-2 mb-2">Social Networks:</p>
+          <button className="flex items-center justify-center py-2 w-full rounded-md gap-2 bg-blue-500 mb-2 px-2">
+            <BsFacebook className="text-white text-xl" />
+            <p className="font-bold text-white">Facebook</p>
+          </button>
+          <button className="flex items-center justify-center py-2 w-full rounded-md gap-2 bg-blue-500 mb-2 px-2">
+            <BsLinkedin className="text-white  text-xl" />
+            <p className="font-bold text-white">Linkedin</p>
+        </button>
+      </div>
       </div>
     </div>
   );
