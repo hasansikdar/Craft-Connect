337bbe242aef794c6400d8c5f2b89fedf3f5c7c9
ifty17 <iftyrahatkabir@gmail.com>
2023-02-09 23:54:49 +0600

    added user name

--- src/Pages/Navbar/NavSearchField.js
+++ src/Pages/Navbar/NavSearchField.js
@@ -5,7 +5,9 @@ import SearchModal from './SearchModal';
 import { Dialog, Transition } from "@headlessui/react";
 // import {} from "react";
 
-const NavSearchField = () => {
+const NavSearchField = ({allusers}) => {
+
+  console.log(allusers?.PhotoURL);
 
 const cancelButtonRef = useRef(null);  
   const [openSearchModal, setOpenSearchModal] = useState(false);
@@ -43,11 +45,11 @@ const cancelButtonRef = useRef(null);
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <div className="fixed inset-0 backdrop-blur-[6px] backdrop-brightness-75" />
+              <div className="fixed inset-0" />
             </Transition.Child>
 
             <div className="fixed inset-0 z-10 overflow-y-auto">
-              <div className="flex min-h-[250px]  justify-center p-4 text-center  mt-12 sm:p-0">
+              <div className="flex min-h-[450px]  justify-center p-4 text-center  mt-12 sm:p-0">
                 <Transition.Child
                   as={Fragment}
                   enter="ease-out duration-100"
@@ -57,9 +59,9 @@ const cancelButtonRef = useRef(null);
                   leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
-                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white dark:bg-[#261b40] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                    <div className="bg-white dark:bg-[#261b40] px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                      <h1 className="py-4 text-left text-sm font-bold text-black dark:text-white">
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <h1 className="py-4 text-left text-sm font-bold text-black">
                         Choose You Photo
                       </h1>
                       <svg
@@ -79,7 +81,22 @@ const cancelButtonRef = useRef(null);
                           d="M6 18L18 6M6 6l12 12"
                         />
                       </svg>
+                    <div>
+                      {
+                        allusers.map((allUser) =>{
+                          return(
+                            <div>
+                                <div>
+                                  <img className="" src={allUser?.PhotoURL} alt="" />
+                                  <p>{allUser?.displayName}</p>
+                                </div>
+                            </div>
+                          )
+                        })
+                      }
                     </div>
+                    </div>
+
                   </Dialog.Panel>
                 </Transition.Child>
               </div>
--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -72,7 +72,6 @@ const Navbar = () => {
     },
     
   });
-  // console.log(allusers);
 
   
 
