182ceea8187b29f40ad80d5cb28201cbdea4bb2b
maruf45 <armaruf450@gmail.com>
2023-02-09 22:10:00 +0600

    lates code update

--- src/Pages/AdCenter/AdCenter.js
+++ src/Pages/AdCenter/AdCenter.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 const AdCenter = () => {
--- src/Pages/AdCenter/AdCenterDetail.js
+++ src/Pages/AdCenter/AdCenterDetail.js
@@ -3,12 +3,26 @@ import { useLoaderData } from 'react-router-dom';
 
 const AdCenterDetail = () => {
     const add = useLoaderData();
-    const {userName} = add;
+    const { userName, userPhoto, advertiseContent, advertiseBg, userEmail } = add;
     console.log(add, userName);
     return (
-        <div className='h-screen flex justify-center items-center'>
-            
-        </div>
+        <section className='h-screen gap-5 flex-col flex mt-12 justify-start w-full items-center overflow-y-auto'>
+            <div>
+                <div className="flex gap-3 items-center">
+                    <img src={userPhoto} alt="userPhoto" className='w-[60px] h-[60px] rounded-full' />
+                    <div className="text-left">
+                        <p className='text-base'>{userName}</p>
+                        <p className='text-xs text-gray-300'>{userEmail}</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={advertiseBg} className='rounded-md shadow-md mt-2 w-full h-[500px]' alt="" />
+                </div>
+                <div>
+                    <p className='mt-5 test-base'>{advertiseContent}</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
