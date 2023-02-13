0b5a571dd84685cca7dbfe07c4dd44b8f2461975
maj-jinnah <maj.jinnah@gmail.com>
2023-02-13 19:48:12 +0600

    query is not working properly

--- src/Pages/BookMarked/BookMarked.js
+++ src/Pages/BookMarked/BookMarked.js
@@ -1,9 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useContext } from "react";
+import { Authcontext } from "../../Context/UserContext";
 
 const BookMarked = () => {
+
+  const { user } = useContext(Authcontext)
+
+  const { data: posts = [], refetch } = useQuery({
+    queryKey: [user?.email],
+    // queryKey: ["posts" ,user?.email],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/user/bookmarkPost/:${user?.email}`);
+      const data = res.json();
+      return data;
+    },
+  });
+
   return (
     <div className="h-screen flex flex-col  items-center mt-5 md:mt-10 ">
       <h1 className="text-xl font-semibold text-black">All your bookmarked posts are here!</h1>
+      
+      {/* <div>
+        {
+          posts.map(post => )
+        }
+      </div> */}
     </div>
   );
 };
---
+++ src/Pages/BookMarked/BookMarkedCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookMarkedCard = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default BookMarkedCard;
