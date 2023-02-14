ff4bea4b727616b2c0c7b2b04ae9ec1968416481
farhanchy99 <farhanchowdhury1999@gmail.com>
2023-02-14 22:02:45 +0600

    ali01

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -128,8 +128,8 @@ const CheckoutForm = ({ openPaymentModal, setOpenPaymentModal, billingDetails })
                                                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                                     <button
                                                         type="submit"
-                                                        className="inline-flex disabled:cursor-not-allowed w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-900 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                                                        disabled={!stripe || !clientSecret || process}
+                                                        className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-900 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                                                        disabled={!stripe || !clientSecret}
                                                     >
                                                         Pay
                                                     </button>
